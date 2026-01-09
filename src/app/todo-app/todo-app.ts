79d922be364f2231fae3a2c979bb0639f9041b56
Mohan Lal <mhnuk2007@gmail.com>
2026-01-09 13:39:11 +0500

    Generate todo-app component

---
+++ src/app/todo-app/todo-app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-app',
+  imports: [],
+  templateUrl: './todo-app.html',
+  styleUrl: './todo-app.css',
+})
+export class TodoApp {
+
+}
