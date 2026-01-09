85bec439f721826026a22eac3292fea1a71952fb
Mohan Lal <mhnuk2007@gmail.com>
2026-01-09 14:02:22 +0500

    Update app template and import todo-app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodoApp } from "./todo-app/todo-app";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TodoApp],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
